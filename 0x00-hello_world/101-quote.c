351d3be633804e978d605db2bfb41b47829e8409
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-02 13:29:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 1 (success)
- *
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+		write(stdi, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 		return (1);
 }
