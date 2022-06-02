553d7bd87f98ed6579280a487e6d573e72c79c98
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-02 12:50:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (sucess)
+ */
+int main(void)
+{
+	write(err, and that piece of art is useful" '-' Dora Korpar, 2015'-'10'-'19\n", 1);
+	return (1);
+}
