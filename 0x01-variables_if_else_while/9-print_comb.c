d473a6ff0639e82ca8e03a7b8c32893fee51211e
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-03 17:09:01 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,19 @@
  */
 int main(void)
 {
-	int i;
+	int i = '0';
 
-	for (i = 0; i <= '9'; i++)
+	while (i <= '9')
+	{
 		putchar(i);
+
+		if (i != '9')
+		{
 			putchar(',');
-				putchar(' ');
+			putchar(' ');
+		}
+		i++;
+	}
 	putchar('\n');
 
 	return (0);
