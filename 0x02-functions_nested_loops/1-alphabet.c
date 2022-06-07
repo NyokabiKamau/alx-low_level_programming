8b35e5cbb1db6648d968ee7bde20416185521b3d
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-07 09:07:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, lower case, by a new line
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; 'a' <= 'z'; a++)
+		_putchar('c');
+	_putchar('\n');
+
+	return (0);
+}
