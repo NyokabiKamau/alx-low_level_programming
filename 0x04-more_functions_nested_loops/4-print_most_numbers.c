3a317750cc9613b210e26b4a5ae08e2b36c469c0
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-09 03:25:54 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,15 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = '0'; i <= '9'; i++)
+	while (i <= '9')
 	{
 		if (!(i == '2' && i == '4'))
+		{
 			_putchar(i);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
