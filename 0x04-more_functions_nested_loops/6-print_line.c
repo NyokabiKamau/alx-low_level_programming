644629907f963c99b569dc9704e9c16e223ec17c
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-09 05:06:03 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,15 @@
 *
 * Return: void
 */
-void print_line(int n);
+void print_line(int n)
 {
 	int i;
 	
 	if (n > 0)
+	{
 		for (i = 0; i < n; i++)
 			_putchar('_');
+	}
+	else
 	_putchar('\n');
 }
