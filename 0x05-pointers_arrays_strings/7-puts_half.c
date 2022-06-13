90cd7cc2035772778c1bc854af00eff9998cea09
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-13 13:14:43 -0700

    ./7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,29 @@
 */
 void puts_half(char *str)
 {
-	int i, j;
+	int n, i, j;
 
 	i = 0;
 	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = (i - 1) / 2;
-	while (j < i)
 	{
-		_putchar(str[j]);
-		j++;
+		i++;
+	}
+
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+		for (n = j; n < i; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		j = (i - 1) / 2;
+		for (n = j + 1; j < i; n++)
+		{
+			_putchar(str[j]);
+		}
 	}
 	_putchar('\n');
 }
