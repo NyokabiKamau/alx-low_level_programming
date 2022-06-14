436cffa96549345a167d7f53f46c7cb08400666f
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-14 11:40:48 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
 * main - generates random valid passwords
 *
@@ -9,5 +8,17 @@
 */
 int main(void)
 {
+	int pswd, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pswd = (rand() % 128);
+		sum += pswd;
+		printf("%c", pswd);
+	}
+	printf("%c", 2772 - sum);
+	
 	return (0);
 }
