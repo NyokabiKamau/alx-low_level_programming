0c5eb61e00e9b32e0e59c9a4e7fe411458ca0995
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-16 07:30:29 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @s: pointer to character
+ *
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char alp[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rt[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		p = 0;
+		for (j = 0; alp[j] != '\0' && p == 0; j++)
+		{
+			 if (s[i] == alp[j])
+			{
+				s[i] = rt[j];
+				p = 1;
+			}
+		}
+	}
+	return (s);
+}
