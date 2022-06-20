cb3142260c51d64a44ba6db6b9a115513013c0d4
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-20 01:10:24 -0700

    0-memset.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; a <= '7'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b <= '8'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
