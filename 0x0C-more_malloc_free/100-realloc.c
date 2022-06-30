2a81ff494ec2da613926cdc1cc035f5137c8fda0
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-06-30 15:54:01 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,17 @@
 
 /**
 * _realloc - reallocates a memory block
+* ptr is prvsly allocated memory pointer with call to malloc: malloc(old_size)
+* old_size is the size, in bytes, of the allocated space for ptr
+* and new_size is the new size, in bytes of the new memory block
+* The contents will be copied to the newly allocated space,
+* in the range from the start of ptr up to the minimum of the old and new sizes
+* If new_size > old_size, the “added” memory should not be initialized
+* If new_size == old_size do not do anything and return ptr
+* If ptr is NULL, then the call is equivalent to malloc(new_size), f
+* or all values of old_size and new_size
+* If new_size=0 and ptr !NULL, then the call is eqvlnt to free(ptr).Return NULL
+* Don’t forget to free ptr when it makes sense
 * @ptr: pointer to the original memory block
 * @old_size: old size of memory block
 * @new_size: new size of memory block
