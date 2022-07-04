cfd911e980a21efb279fd27ed4a7556ef4be7bd3
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-04 16:23:34 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information data
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: gives detailed information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
