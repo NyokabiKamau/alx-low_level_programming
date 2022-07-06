1ec17e9cccd2c2604c7411022deed44ddc07bf3b
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-06 15:42:08 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0 || !array || !cmp)
 		return (-1);
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if(cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
+	return (-1);
 }
