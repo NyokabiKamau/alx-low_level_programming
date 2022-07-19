41919c1833654abb2bc813a308481b7c3a1fb174
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-19 08:56:13 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,21 +9,21 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
-	long int diff;
+	long int a;
 
 	while (head)
 	{
-		diff = head - head->next;
+		a = head - head->next;
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
+		if (a > 0)
 			head = head->next;
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 			exit(98);
 		}
 	}
-
 	return (num);
 }
