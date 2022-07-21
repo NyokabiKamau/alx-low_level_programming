6e997956be3dc7335b6f95ee27bd66c5f1a015da
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-21 19:16:03 +0000

    100-get_endianness.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * set_bit - sets value of bit to 1 at given index.
- * @n: pointer of an unsigned long int.
+ * @n: pointer of number to change
  * @index: index starting from 0 of the bit to set
  *
  * Return: 1 if it worked, -1 if an error
