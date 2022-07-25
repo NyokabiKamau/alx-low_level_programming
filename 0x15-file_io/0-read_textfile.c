41aed330683d05097ae59433c42b6da8bf1828a2
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-25 16:05:46 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t x, y;
 	char *buf;
 
-	if ((filename == NULL) || (!filename))
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
