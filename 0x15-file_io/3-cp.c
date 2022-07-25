eb67c96b869510e1e82f9114743094bc1881d0c2
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-25 19:14:39 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * error - checks if files can be opened
+ * error_file - checks if files can be opened
  * @file_from: file content to be copied from
  * @file_to: file content to be copied
  * @argv: array of arguments
  *
  * Return: nothing
  */
-void error(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_from == -1)
+	if (file_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)
+	if (file_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: cant write to %s\n", argv[2]);
 		exit(99);
@@ -38,32 +38,32 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from filr_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
 	x = 1024;
 	while (x == 1024)
 	{
 		x = read(file_from, buf, 1024);
 		if (x < 0)
-			error(-1, 0, argv);
+			error_file(-1, 0, argv);
 		y = write(file_to, buf, x);
 		if (y < 0)
-			error(0, -1, argv);
+			error_file(0, -1, argv);
 	}
 	err_close = close(file_from);
 	if (err_close < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	err_close = close(file_to);
 
+	err_close = close(file_to);
 	if (err_close < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
